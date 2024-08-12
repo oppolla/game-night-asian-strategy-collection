b354783ce07897466aced5ffa2aaece4422b459c
Andy <aeudet@gmail.com>
2024-08-11 20:02:40 -0400

    fixes

--- Contents/mods/game-night-shogi/media/lua/server/gameNight - DistributionsShogi.lua
+++ Contents/mods/game-night-shogi/media/lua/server/gameNight - DistributionsShogi.lua
@@ -16,6 +16,7 @@ gameNightDistro.gameNightBoxes["ShogiBoard"] = {
         "ShogiFuhyo1", 9999,
         "ShogiFuhyo1", 9999,
         "ShogiFuhyo1", 9999,
+
         "ShogiFuhyo2", 9999,
         "ShogiFuhyo2", 9999,
         "ShogiFuhyo2", 9999,
@@ -25,28 +26,35 @@ gameNightDistro.gameNightBoxes["ShogiBoard"] = {
         "ShogiFuhyo2", 9999,
         "ShogiFuhyo2", 9999,
         "ShogiFuhyo2", 9999,
+
         "ShogiKyosha1", 9999,
         "ShogiKyosha1", 9999,
         "ShogiKyosha2", 9999,
         "ShogiKyosha2", 9999,
+
         "ShogiKeima1", 9999,
         "ShogiKeima1", 9999,
         "ShogiKeima2", 9999,
         "ShogiKeima2", 9999,
+
         "ShogiKinsho1", 9999,
         "ShogiKinsho1", 9999,
         "ShogiKinsho2", 9999,
         "ShogiKinsho2", 9999,
+
         "ShogiGinsho1", 9999,
         "ShogiGinsho1", 9999,
         "ShogiGinsho2", 9999,
         "ShogiGinsho2", 9999,
-        "ShogiShogiKakugyo1", 9999,
-        "ShogiShogiKakugyo2", 9999,
-        "ShogiShogiHisha1", 9999,
-        "ShogiShogiHisha2", 9999,
-        "ShogiShogiOsho1", 9999,
-        "ShogiShogiOsho2", 9999,
+
+        "ShogiKakugyo1", 9999,
+        "ShogiKakugyo2", 9999,
+
+        "ShogiHisha1", 9999,
+        "ShogiHisha2", 9999,
+
+        "ShogiOsho1", 9999,
+        "ShogiOsho2", 9999,
       },
     junk = { rolls = 1, items = {} }, fillRand = 0,
 }
