bbeb35229fb68f3579717f130874b25ba2340c08
Andy <aeudet@gmail.com>
2024-08-11 20:03:42 -0400

    Update gameNight - DistributionsJanggi.lua

--- Contents/mods/game-night-janggi/media/lua/server/gameNight - DistributionsJanggi.lua
+++ Contents/mods/game-night-janggi/media/lua/server/gameNight - DistributionsJanggi.lua
@@ -9,26 +9,32 @@ gameNightDistro.gameNightBoxes["JanggiBoard"] = {
     items = {
         "JanggiRedGeneral", 9999,
         "JanggiBlueGeneral", 9999,
+
         "JanggiRedHorse", 9999,
         "JanggiRedHorse", 9999,
         "JanggiBlueHorse", 9999,
         "JanggiBlueHorse", 9999,
+
         "JanggiRedElephant", 9999,
         "JanggiRedElephant", 9999,
         "JanggiBlueElephant", 9999,
         "JanggiBlueElephant", 9999,
+
         "JanggiRedChariot", 9999,
         "JanggiRedChariot", 9999,
         "JanggiBlueChariot", 9999,
         "JanggiBlueChariot", 9999,
+
         "JanggiRedCannon", 9999,
         "JanggiRedCannon", 9999,
         "JanggiBlueCannon", 9999,
         "JanggiBlueCannon", 9999,
+
         "JanggiRedGuard", 9999,
         "JanggiRedGuard", 9999,
         "JanggiBlueGuard", 9999,
         "JanggiBlueGuard", 9999,
+        
         "JanggiRedSoldier", 9999,
         "JanggiRedSoldier", 9999,
         "JanggiRedSoldier", 9999,
